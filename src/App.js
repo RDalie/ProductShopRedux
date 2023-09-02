94174e68c01f723ff3ab08a7504c7efd05de9b54
Rabya Tayal <rabyatayal1201@gmail.com>
2023-09-02 18:29:49 +1000

    Finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-import Header from './containers/Header';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProductListing/>} />
+          <Route path="product/:productId" element={<ProductDetail/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -2,17 +2,27 @@ import { ActionTypes } from "../constants/action-types";
 
 //Reducer always takes the initial state and the action
 const initialState = {products: [
-    {
-        id: 1,
-        title: "Rabya",
-        category: "programmer"
-    }
+
 ]};
 export const productReducer = (state = initialState, {type, payload}) => {
     switch(type) {
         case ActionTypes.SET_PRODUCTS:
-            return state;
+            return {...state, products:payload };
         default:
             return state
     } 
+}
+
+export const selectedProductReducer = (state={} , {type, payload}) => {
+    switch (type) {
+        case ActionTypes.SELECTED_PRODUCT:
+            return {...state, ...payload};
+            break;
+        case ActionTypes.REMOVE_SELECTED_PRODUCT:
+            return {...state, ...payload};
+            break;      
+        default:
+            return state;
+            break;
+    }
 }
